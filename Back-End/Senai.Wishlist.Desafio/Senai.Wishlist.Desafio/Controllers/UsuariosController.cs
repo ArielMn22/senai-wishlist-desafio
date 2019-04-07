7a6b4d6a88d300786a3e9d865333f885bced4483
ArielMn22 <arielpaixao10@gmail.com>
2019-04-07 01:02:51 -0300

    Adicionando projeto C#

---
+++ Back-End/Senai.Wishlist.Desafio/Senai.Wishlist.Desafio/Controllers/UsuariosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Senai.Wishlist.Desafio.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet]
+        //[Authorize]
+        public IActionResult ListarUsuarios()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    mensagem = "Erro: " + ex
+                });
+            }
+        }
+    }
+}
---
+++ Back-End/Senai.Wishlist.Desafio/Senai.Wishlist.Desafio/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Wishlist.Desafio.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 5, ErrorMessage = "Senha deve conter entre 5 e 255 caractéres."]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5, ErrorMessage = "Senha deve conter entre 5 e 255 caractéres."]
+        public string Senha { get; set; }
+    }
+}
