15d9f450037ca68e99555ada9cdaf78e3217531f
Cândida Rosa Paraizo <43765601896@info.local>
2019-04-08 16:06:56 -0300

    Acrescentando prints para documentação

--- Banco de Dados/Scripts/Script_Manipulação.sql
+++ Banco de Dados/Scripts/Script_Manipulação.sql
@@ -14,9 +14,9 @@ INSERT INTO DESEJOS (ID_VERBO, DESCRICAO, DATA_CRIACAO, ID_USUARIO)
 VALUES   ('1','Comprar PC da NASA.','07/04/2019','1')
 		,('6','Não sei o que desejar!','06/04/2019','2')
 		,('5','Ser famoso.','05/10/2018','3')
-		,('1','Comprar um caminhão ','15/12/2018','1')
-		,('1','Comprar um triciclo','01/01/2017','2')
-		,('3','Viajar para Ubatuba','05/05/2017','3')
+		,('1','Comprar um caminhão.','15/12/2018','1')
+		,('1','Comprar um triciclo.','01/01/2017','2')
+		,('3','Viajar para Ubatuba.','05/05/2017','3')
 
 
 SELECT * FROM VERBOS
@@ -25,8 +25,7 @@ SELECT * FROM DESEJOS
 
 
 
-
------Sessão DELETE:
+-----Sessão DELETE:-----
 DELETE FROM DESEJOS
 DELETE FROM USUARIOS
 DELETE FROM VERBOS
