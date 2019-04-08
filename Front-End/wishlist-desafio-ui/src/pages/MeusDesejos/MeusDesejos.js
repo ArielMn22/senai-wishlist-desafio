121ffea80ec4614c22da7484b1dcf2563e9777d9
Ariel Paixão Dos Santos <33747821839@info.local>
2019-04-08 16:44:55 -0300

    Criando Meus Desejos

--- Front-End/wishlist-desafio-ui/src/pages/MeusDesejos/MeusDesejos.js
+++ Front-End/wishlist-desafio-ui/src/pages/MeusDesejos/MeusDesejos.js
@@ -3,7 +3,8 @@ import Axios from 'axios';
 
 export default class MeusDesejos extends Component {
     buscarMinhasConsultas() {
-        Axios.get('http://localhost:5000/api/desejos/' + localStorage.getItem("usuarioId"))
+        // Axios.get('http://localhost:5000/api/desejos/' + localStorage.getItem("usuarioNome"))
+        Axios.get('http://localhost:5000/api/desejos/ariel')
         .then(data => {
             console.log(data)
             // carregarTabela(data);
