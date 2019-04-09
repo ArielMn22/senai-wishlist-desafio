4725f63d8384d8c77aed66526b6633e2f5ba2c2e
Ariel Paixão Dos Santos <33747821839@info.local>
2019-04-09 15:37:58 -0300

    Adicionando Meus Desejos

--- Front-End/wishlist-desafio-ui/src/pages/MeusDesejos/MeusDesejos.js
+++ Front-End/wishlist-desafio-ui/src/pages/MeusDesejos/MeusDesejos.js
@@ -1,21 +1,88 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
+import Cabecalho from '../Components/Cabecalho'
+import '../../assets/css/login.css'
+import '../../assets/css/desejos.css'
 
 export default class MeusDesejos extends Component {
+
+    constructor () {
+        super();
+
+        this.state = {
+            lista: [],
+            id: "",
+            descricao: "",
+            dataCriacao: "",
+            categoria: "",
+            autor: ""
+        };
+    }
+
     buscarMinhasConsultas() {
         // Axios.get('http://localhost:5000/api/desejos/' + localStorage.getItem("usuarioNome"))
-        Axios.get('http://localhost:5000/api/desejos/ariel')
+        Axios.get('http://192.168.3.143:5000/api/desejos/')
         .then(data => {
-            console.log(data)
+            console.log(data);
+            this.setState({ lista : data.data});
             // carregarTabela(data);
         })
-        .catch(erro => console.log(erro))
+        // .catch(erro => console.log(erro))
+    }
+
+    componentDidMount()
+    {
+        this.buscarMinhasConsultas();
     }
 
     render(){
         return(
-            // HTML
-            <h1>It Works!</h1>
+            <div>
+            <Cabecalho />
+                <main>
+                    <section id="filterUser" className="pa-all-g">
+                        <h2>Filtrar por Categoria</h2>
+                        <form action="" id="formUser">
+                            <select name="categorias" id="categoriasSelect">
+                                <option value="comprar">
+                                    Comprar
+                                </option>
+                                <option value="comprar">
+                                    Viajar
+                                </option>
+                            </select>
+                            <label for="">
+                                <input className="btn-new" value="Filtrar" type="submit" id="submitBtn" name="submit" />
+                            </label>
+                        </form>
+                    </section>
+                    <section id="tabelaDesejos" className="pa-all-g">
+                        {/* <!-- <div className="tabelaContent"> --> */}
+                        <h2>Meus Desejos</h2>
+                        <table className="ma-top-g">
+                            <tr id="head">
+                                <th>#</th>
+                                <th>Descrição</th>
+                                <th>Data de Criação</th>
+                                <th>Categoria</th>
+                                <th>Autor</th>
+                            </tr>
+                            {this.state.lista.map(function (desejo) {
+                                return(
+                                    <tr>
+                                        <td>{desejo.id}</td>
+                                        <td>{desejo.descricao}</td>
+                                        <td>{desejo.dataCriacao}</td>
+                                        <td>{desejo.verboNome}</td>
+                                        <td>{desejo.usuarioNome}</td>
+                                    </tr>
+                                );
+                            })}
+                        </table>
+                        {/* <!-- </div> --> */}
+                    </section>
+                </main>
+            </div>
         );
     }
 }
---
+++ Front-End/wishlist-desafio-ui/src/pages/TodosDesejos/TodosDesejos.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import Axios from 'axios';
+import Cabecalho from '../Components/Cabecalho'
+import '../../assets/css/login.css'
+import '../../assets/css/desejos.css'
+
+export default class TodosDesejos extends Component {
+    return()
+}
